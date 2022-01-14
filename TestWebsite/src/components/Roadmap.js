8b42e1f0daf7d47d5b10a8b5f84e98bc3b73adda
SeanConnorz <seanandhughes@gmail.com>
2022-01-14 14:41:25 -0500

    Adding css to Roadmap component

--- TestWebsite/src/components/Roadmap.js
+++ TestWebsite/src/components/Roadmap.js
@@ -4,15 +4,27 @@ import "../styles/Roadmap.css";
 export default function Roadmap() {
   return (
     <section className="roadmap">
-      <div className="lurrie-contaier">
+      <div className="lurry-container">
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <src></src>
+          <src></src>
+          <src></src>
+          <src></src>
         </ul>
       </div>
-      <div className="roadmap-description">Lorem ipsum</div>
+      <div className="roadmap-description">
+        <h1>Roadmap & utility</h1>
+        <div>
+          <ol>
+            <p>Step 1: Launch Floatie Lurry Collection</p>
+            <p>Step 2: Get listed on Secondary marketplaces</p>
+            <p>
+              Step 3: Marketing - setup Discord, Telegram, Reddit and Twitter
+              channels
+            </p>
+          </ol>
+        </div>
+      </div>
     </section>
   );
 }
