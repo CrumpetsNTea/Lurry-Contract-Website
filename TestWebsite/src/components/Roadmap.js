3af2bdbb9d54ed85bf2f07f7b10af12e6024a62e
SeanConnorz <seanandhughes@gmail.com>
2022-01-14 16:58:38 -0500

    Adding lurry images

--- TestWebsite/src/components/Roadmap.js
+++ TestWebsite/src/components/Roadmap.js
@@ -4,29 +4,32 @@ import "../styles/Roadmap.css";
 export default function Roadmap() {
   return (
     <section className="roadmap">
-      <div className="lurry-container">
-        <ul>
-          <src></src>
-          <src></src>
-          <src></src>
-          <src></src>
-        </ul>
-      </div>
+      <ul className="lurry-container">
+        <img className="lurry-png" src="../../images/3DMovieLurry.png"></img>
+        <img className="lurry-png" src="../../images/BiohazardLurry.png"></img>
+        <img className="lurry-png" src="../../images/CrayonLurry.png"></img>
+        <img
+          className="lurry-png"
+          src="../../images/ForgotTheSunscreenLurry.png"
+        ></img>
+      </ul>
       <div className="roadmap-description">
-        <h1 className="roadmap-description-header">ROADMAP & UTILITY</h1>
-        <div className="roadmap-description-steps">
-          <h4 className="description-steps">
-            <span className="bold">Step 1: </span>
-            Launch Floatie Lurry Collection
-          </h4>
-          <h4 className="description-steps">
-            <span className="bold">Step 2: </span>
-            Get listed on Secondary marketplaces
-          </h4>
-          <h4 className="description-steps">
-            <span className="bold">Step 1: </span>
-            Marketing - setup Discord, Telegram, Reddit and Twitter channels
-          </h4>
+        <div className="roadmap-description-info">
+          <h1 className="roadmap-description-header">ROADMAP & UTILITY</h1>
+          <div className="roadmap-description-steps">
+            <h4 className="description-steps">
+              <span className="bold">Step 1: </span>
+              Launch Floatie Lurry Collection
+            </h4>
+            <h4 className="description-steps">
+              <span className="bold">Step 2: </span>
+              Get listed on Secondary marketplaces
+            </h4>
+            <h4 className="description-steps">
+              <span className="bold">Step 3: </span>
+              Marketing - setup Discord, Telegram, Reddit and Twitter channels
+            </h4>
+          </div>
         </div>
       </div>
     </section>
