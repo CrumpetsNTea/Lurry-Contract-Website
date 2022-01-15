ccd25246e0f46e05ded578a185b2b7cf6972fa0a
SeanConnorz <seanandhughes@gmail.com>
2022-01-14 20:04:42 -0500

    Adding carousel stal ststate logic

--- TestWebsite/src/components/Roadmap.js
+++ TestWebsite/src/components/Roadmap.js
@@ -1,18 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Roadmap.css";
 
 export default function Roadmap() {
+  const [slide, setSlide] = useState(0);
+  const [transition, setTransition] = useState("slide-left");
+
+  const increaseSlide = () => {
+    if (slide < 2) {
+      setTransition("slide-right-out");
+      setTimeout(() => {
+        setTransition("slide-right");
+        setSlide(slide + 1);
+      }, 50);
+    }
+  };
+
+  const decreaseSlide = () => {
+    if (slide > 0) {
+      setTransition("slide-left-out");
+      setTimeout(() => {
+        setTransition("slide-left");
+        setSlide(slide - 1);
+      }, 50);
+    }
+  };
+
   return (
     <section className="roadmap">
-      <ul className="lurry-container">
-        <img className="lurry-png" src="../../images/3DMovieLurry.png"></img>
-        <img className="lurry-png" src="../../images/BiohazardLurry.png"></img>
-        <img className="lurry-png" src="../../images/CrayonLurry.png"></img>
-        <img
-          className="lurry-png"
-          src="../../images/ForgotTheSunscreenLurry.png"
-        ></img>
-      </ul>
+      {slide === 0 && (
+        <ul className="lurry-container">
+          <button onClick={decreaseSlide} className="carousel-arrow">
+            Click me
+          </button>
+          <div className={transition}>
+            <img
+              className="lurry-png"
+              src="../../images/3DMovieLurry.png"
+            ></img>
+            <img
+              className="lurry-png"
+              src="../../images/BiohazardLurry.png"
+            ></img>
+            <img className="lurry-png" src="../../images/CrayonLurry.png"></img>
+            <img
+              className="lurry-png"
+              src="../../images/ForgotTheSunscreenLurry.png"
+            ></img>
+          </div>
+          <button onClick={increaseSlide} className="carousel-arrow">
+            Click me
+          </button>
+        </ul>
+      )}
+      {slide === 1 && (
+        <ul className="lurry-container">
+          <button onClick={decreaseSlide} className="carousel-arrow">
+            Click me
+          </button>
+          <div className={transition}>
+            <img
+              className="lurry-png"
+              src="../../images/GentlemanLurry.png"
+            ></img>
+            <img className="lurry-png" src="../../images/Lurry.png"></img>
+            <img
+              className="lurry-png"
+              src="../../images/LurryinDisguise.png"
+            ></img>
+            <img className="lurry-png" src="../../images/SailorLurry.png"></img>
+          </div>
+          <button onClick={increaseSlide} className="carousel-arrow">
+            Click me
+          </button>
+        </ul>
+      )}
+      {slide === 2 && (
+        <ul className="lurry-container">
+          <button onClick={decreaseSlide} className="carousel-arrow">
+            Click me
+          </button>
+          <div className={transition}>
+            <img
+              className="lurry-png"
+              src="../../images/StrawburryLurry.png"
+            ></img>
+            <img
+              className="lurry-png"
+              src="../../images/BiohazardLurry.png"
+            ></img>
+            <img className="lurry-png" src="../../images/CrayonLurry.png"></img>
+            <img
+              className="lurry-png"
+              src="../../images/ForgotTheSunscreenLurry.png"
+            ></img>
+          </div>
+          <button onClick={increaseSlide} className="carousel-arrow">
+            Click me
+          </button>
+        </ul>
+      )}
       <div className="roadmap-description">
         <div className="roadmap-description-info">
           <h1 className="roadmap-description-header">ROADMAP & UTILITY</h1>
