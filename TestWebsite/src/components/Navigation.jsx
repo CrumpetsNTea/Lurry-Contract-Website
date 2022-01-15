980cd4d388fc9bfaeb62d0378f38cab906875e9a
iouri volkov <volk.iouri@gmail.com>
2022-01-15 17:43:59 -0500

    add css for navbar

--- TestWebsite/src/components/Navigation.jsx
+++ TestWebsite/src/components/Navigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/Navigation.css';
 
 
 export default function Navigation() {
 
   return (
     <div className="navbar">
-      <img src="/images/transparent logo.png" alt="Secret Lurry Society Logo" className="logo"/>
+      <img src="/images/SLSlogo.png" alt="Secret Lurry Society Logo" className="logo"/>
       <section className="nav-items">
         <a href="/about">About</a>
         <a href="/roadmap">Roadmap</a>
