be03dfe137becf27f7ccb5d1aa54c22efc8bafc0
Iouri Volkov <volk.iouri@gmail.com>
2022-01-15 22:58:13 -0500

    start building QuestionList component

--- LurryMintSite/src/components/QuestionList.jsx
+++ LurryMintSite/src/components/QuestionList.jsx
@@ -1,10 +1,36 @@
 // Contains FAQ section components
 
-import React from 'react';
+import React, {useState} from 'react';
+
 
 function QuestionList() {
 
-  
+  const [faqs, setFaqs] = useState([
+    {
+      question: "What are NFTs?",
+      answer: "NFT's - short for Non-Fungible Tokens, are assets stored on the blockchain the ownership and authenticity of which is publically verifiable.",
+      open: true
+    },
+    {
+      question: "How can I mint a Floatie Lurry?",
+      answer: "video",
+      open: false
+    },
+    {
+      question: "What can I do with my Floatie Lurry?",
+      answer: "Simply put, anything. However if you're looking for a more, as Lurry would say (or quack) technical answer - once minted, you retain full ownership rights over your Floatie Lurry. Go ahead and talk to it about all those bugs you found in your code or show it off to your friends and family!"
+    }
+  ])
+
+return (
+  <div className="faqs-list">
+
+
+  </div>
+
+
+)
+
 
 }
 
---
+++ LurryMintSite/src/components/Questions.jsx
@@ -0,0 +1,14 @@
+import React from React;
+import QuestionList from './QuestionList';
+import TeamList from './TeamList';
+import Footer from './Footer';
+
+function Questions() {
+
+  return (
+    <div></div>
+  )
+}
+
+
+export default Questions;
