ad7e90402d1c1d5c75994a24eb872e00864fee9b
Iouri Volkov <volk.iouri@gmail.com>
2022-01-16 13:07:52 -0500

    styling changes

--- LurryMintSite/src/components/QuestionList.jsx
+++ LurryMintSite/src/components/QuestionList.jsx
@@ -4,7 +4,7 @@ import React, {useState} from 'react';
 
 
 function QuestionList() {
-
+  // set default state to questions and answers - set one card to open  
   const [faqs, setFaqs] = useState([
     {
       question: "What are NFTs?",
@@ -13,26 +13,26 @@ function QuestionList() {
     },
     {
       question: "How can I mint a Floatie Lurry?",
-      answer: "video",
+      answer: "First click on the 'Connect' button and sign the transaction with your Metamask. This first transaction will authenticate you. Next, click on the 'Mint' button when you are ready to mint a Floatie Lurry. A Metamask popup will appear prompting you to confirm the transaction. Once you are ready to proceed, click approve. The Floatie Lurry will be kept in your Metamask wallet and can be seen on the OpenSea testnet marketplace.",
       open: false
     },
     {
       question: "What can I do with my Floatie Lurry?",
-      answer: "Simply put, anything. However if you're looking for a more, as Lurry would say (or quack) technical answer - once minted, you retain full ownership rights over your Floatie Lurry. Go ahead and talk to it about all those bugs you found in your code or show it off to your friends and family!",
+      answer: "Simply put, anything. However if you're looking for a more, as Lurry would say (or quack) technical answer - once minted, you retain full ownership rights over your Floatie Lurry. Go ahead and talk to it about all those bugs you found in your code or show it off to your programming buddies!",
       open: false
     }
   ]);
 
 return (
-  <div>
+  <div className="faq-section">
     <h1>FAQ Section </h1>
     <div className="faqs-container">
-      {faqs.map((faq, i) => (
-        <div>{faqs.question}<br></br>
-        </div>
+      {faqs.map((faq, index) => (
+        <div>
+       {faqs.question}<br/>
+       </div>
       ))}
     </div>
-
   </div>
 )
 }
