990af4179e3e430c1c24962467c40fd44c10811c
iouri volkov <volk.iouri@gmail.com>
2022-01-15 20:20:10 -0500

    navbar styling changes

--- TestWebsite/src/components/Navigation.jsx
+++ TestWebsite/src/components/Navigation.jsx
@@ -12,9 +12,9 @@ export default function Navigation() {
       <img src="/images/SLSlogo.png" alt="Secret Lurry Society Logo" className="logo"/>
       <nav>
         <ul className="nav-links">
-          <li><a href="#">About</a></li>
-          <li><a href="#">Roadmap</a></li>
-          <li><a href="#">FAQ</a></li>
+          <li><a href="About">About</a></li>
+          <li><a href="Roadmap">Roadmap</a></li>
+          <li><a href="Faq">FAQ</a></li>
         </ul>
       </nav>
       <a className="mint" href="#">
