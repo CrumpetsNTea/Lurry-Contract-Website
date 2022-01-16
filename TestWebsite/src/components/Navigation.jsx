49efd26bb2dd4128505a2bf36b839c196f94d641
iouri volkov <volk.iouri@gmail.com>
2022-01-15 19:17:36 -0500

    finish navbar css

--- TestWebsite/src/components/Navigation.jsx
+++ TestWebsite/src/components/Navigation.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import '../styles/Navigation.css';
 
 
+
 export default function Navigation() {
 
   return (
+    <>
     <div className="navbar">
       <img src="/images/SLSlogo.png" alt="Secret Lurry Society Logo" className="logo"/>
-      <section className="nav-items">
-        <a href="/about">About</a>
-        <a href="/roadmap">Roadmap</a>
-        <a href="/faq">FAQ</a>
-      </section>
+      <nav>
+        <ul className="nav-links">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Roadmap</a></li>
+          <li><a href="#">FAQ</a></li>
+        </ul>
+      </nav>
+      <a className="mint" href="#">
+        <button>Mint!</button>
+      </a>
       </div>
+    </>
   );
 };
 
