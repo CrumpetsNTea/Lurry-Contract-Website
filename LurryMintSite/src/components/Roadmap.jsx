91582438f9338baa4306136189397aadb7b27acc
CrumpetsNTea <t.botell@gmail.com>
2022-01-22 15:17:44 -0500

    Add responsive Lurry carousel

--- LurryMintSite/src/components/Roadmap.jsx
+++ LurryMintSite/src/components/Roadmap.jsx
@@ -1,115 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
+import LurryCarousel from "./Carousel";
 import "../styles/Roadmap.css";
 
 export default function Roadmap() {
-  const [slide, setSlide] = useState(1);
-  const [transition, setTransition] = useState("slide-left");
-
-  // Animation when increasing lurry slide
-  const increaseSlide = () => {
-    if (slide < 2) {
-      setTransition("slide-left-out");
-      setTimeout(() => {
-        setTransition("slide-left");
-        setSlide(slide + 1);
-      }, 50);
-    }
-  };
-
-  // Animation when decreasing lurry slide
-  const decreaseSlide = () => {
-    if (slide > 0) {
-      setTransition("slide-right-out");
-      setTimeout(() => {
-        setTransition("slide-right");
-        setSlide(slide - 1);
-      }, 50);
-    }
-  };
 
   return (
+    <>
     <section className="roadmap">
-      <ul className="lurry-container">
-        <button onClick={decreaseSlide} className="carousel-arrow">
-          <img
-            className="mirrored arrow"
-            src="../../images/Icon-arrow-right-white.png"
-          ></img>
-        </button>
-        {slide === 0 && (
-          <div className="contain-slide">
-            <div className={transition}>
-              <img
-                className="lurry-png large-only"
-                src="../../images/3DMovieLurry.png"
-              ></img>
-              <img
-                className="lurry-png medium-large-only"
-                src="../../images/BiohazardLurry.png"
-              ></img>
-              <img
-                className="lurry-png small-excluded"
-                src="../../images/CrayonLurry.png"
-              ></img>
-              <img
-                className="lurry-png"
-                src="../../images/ForgotTheSunscreenLurry.png"
-              ></img>
-            </div>
-          </div>
-        )}
-        {slide === 1 && (
-          <div className="contain-slide">
-            <div className={transition}>
-              <img
-                className="lurry-png large-only"
-                src="../../images/GentlemanLurry.png"
-              ></img>
-              <img
-                className="lurry-png medium-large-only"
-                src="../../images/Lurry.png"
-              ></img>
-              <img
-                className="lurry-png small-excluded"
-                src="../../images/LurryinDisguise.png"
-              ></img>
-              <img
-                className="lurry-png"
-                src="../../images/SailorLurry.png"
-              ></img>
-            </div>
-          </div>
-        )}
-        {slide === 2 && (
-          <div className="contain-slide">
-            <div className={transition}>
-              <img
-                className="lurry-png large-only"
-                src="../../images/StrawburryLurry.png"
-              ></img>
-              <img
-                className="lurry-png medium-large-only"
-                src="../../images/BiohazardLurry.png"
-              ></img>
-              <img
-                className="lurry-png small-excluded"
-                src="../../images/CrayonLurry.png"
-              ></img>
-              <img
-                className="lurry-png"
-                src="../../images/ForgotTheSunscreenLurry.png"
-              ></img>
-            </div>
-          </div>
-        )}
-        <button onClick={increaseSlide} className="carousel-arrow">
-          <img
-            className="arrow"
-            src="../../images/Icon-arrow-right-white.png"
-          ></img>
-        </button>
-      </ul>
-
+    <LurryCarousel></LurryCarousel>
       <div className="roadmap-description" id="Roadmap">
         <div className="roadmap-description-info">
           <h1 className="roadmap-description-header">ROADMAP & UTILITY</h1>
@@ -131,5 +29,6 @@ export default function Roadmap() {
         </div>
       </div>
     </section>
+    </>
   );
 }
